e5897fd6a8ccbe3f3ebe2e8b1d42f8c3b4a12ca0
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-05-06 12:23:12 -0500

    movies api push

--- src/main/resources/movies-create-script.sql
+++ src/main/resources/movies-create-script.sql
@@ -1,26 +1,69 @@
-# 1. go get the json file from glitch
-# 2. copypasta into a new json file under /resources
+CREATE DATABASE IF NOT EXISTS movies_db;
 
-# --> You may need to establish a connection to your localhost db here
+USE movies_db;
+#DROP TABLES WITH DEPENDENCIES
 
-# 3. create the movies_db
+DROP TABLE IF EXISTS movie_genre;
+DROP TABLE IF EXISTS movie_actor;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS directors;
 
-# 4. use the movies_db
 
-# 5. drop the table(s) to which no other tables are dependent (none at first)
+CREATE TABLE IF NOT EXISTS directors
+(
+    id            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    director_name VARCHAR(255),
+    PRIMARY KEY (id)
+);
 
-# 6. map the json movie properties to movies table columns
-# --> start with just a movies table with all the columns found in the movie json properties
+CREATE TABLE IF NOT EXISTS genres
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32),
+    PRIMARY KEY (id)
+);
 
-# 6a. Run the script to make sure it works
 
-# 7. refactor to extract the directors to a new table with just an id and name
-# --> change the movies table to reference the directors table via Foreign Key
-# --> now that movies is dependent on directors, you need to move directors above movies in the script
+Create Table IF NOT EXISTS movies
+(
+    id            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    movie_title   varchar(120),
+    plot          TEXT,
+    year_released CHAR(4),
+    poster        TEXT,
+    rating        CHAR(1),
+    director_id   INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (director_id) REFERENCES directors (id)
+);
 
-# 8. Go add DROP IF EXIST statements for movies and directors
+CREATE TABLE IF NOT EXISTS movie_genre
+(
+    movie_id INT UNSIGNED NOT NULL,
+    genre_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies (id),
+    FOREIGN KEY (genre_id) REFERENCES genres (id)
+);
 
-# 9. RUN IT!
+CREATE
+    TABLE IF NOT EXISTS actors
+(
+
+    id         INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    actor_name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS movie_actor
+(
+    movie_id INT UNSIGNED NOT NULL,
+    actor_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies (id),
+    FOREIGN KEY (actor_id) REFERENCES actors (id)
+);
+
 
 
 
