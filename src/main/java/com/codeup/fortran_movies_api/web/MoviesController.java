92588eb0b0603ceddc913f7c5818fdef906c9a18
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-05-11 09:18:37 -0500

    push changes from yesterday

--- src/main/java/com/codeup/fortran_movies_api/web/MoviesController.java
+++ src/main/java/com/codeup/fortran_movies_api/web/MoviesController.java
@@ -1,19 +1,18 @@
 package com.codeup.fortran_movies_api.web;
 
-import com.codeup.fortran_movies_api.data.Movie;
-import com.codeup.fortran_movies_api.data.MoviesRepository;
-import org.springframework.web.bind.annotation.*;
+        import com.codeup.fortran_movies_api.data.Movie;
+        import com.codeup.fortran_movies_api.data.MoviesRepository;
+        import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.http.HttpServletResponse;
+        import java.io.IOException;
+        import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/movies")
 public class MoviesController {
-
-    private List<Movie> sampleMovies = setMovies();
-
 
     private final MoviesRepository moviesRepository;
 
@@ -21,72 +20,52 @@ public class MoviesController {
         this.moviesRepository = moviesRepository;
     }
 
-
-
-
-
-    @GetMapping
-    public Movie one() {
-        return sampleMovies.get(1);
-    }
-
-    @GetMapping("all") // Path becomes: /api/movies/all
+    // TODO: put the expected path out to the side of the method annotation
+    //  -> this helps to keep track so we don't have to guess if methods conflict on the same path
+    @GetMapping("all") // /api/movies/all
     public List<Movie> getAll() {
-        return sampleMovies;
+        return moviesRepository.findAll(); // TODO: findAll() will return a list of objects and is provided by the JpaRepository
     }
 
+    // /api/movies/3 <- 3 is the path variable for id
     @GetMapping("{id}") // Define the path variable to use here
     public Movie getById(@PathVariable int id) { // Actually use the path variable here by annotating a parameter with @PathVariable
-        return sampleMovies.stream().filter((movie) -> {
-                    return movie.getId() == id; // filter out non-matching movies
-                })
-                .findFirst() // isolate to first match
-                .orElse(null); // prevent errors by returning null... not the greatest practice, but it'll do for now
+        return moviesRepository.findById(id).orElse(null); // prevent errors by returning null... not the greatest practice, but it'll do for now
     }
 
-    @GetMapping("search")
-    public Movie getByTitle (@RequestParam String title){
-        Movie movieToReturn = null;
-
-        for (Movie movie : sampleMovies){
-            if (movie.getTitle().equals(title)){
-                movieToReturn = movie;
-            }
-        }
-        return movieToReturn;
+    @GetMapping("search") // /api/movies/search?title=<movieTitle>
+    public List<Movie> getByTitle(@RequestParam("title") String title){
+        // TODO: we need to create the findByTitle() method in our MoviesRepository - magic!
+        return moviesRepository.findByTitle(title);
     }
 
-    @PostMapping
+    @GetMapping("search/year") // api/movies/search/year
+    public List<Movie> getByYearRange(@RequestParam("startYear") int startYear, @RequestParam("endYear") int endYear){
+        // TODO: @RequestParam expects a query parameter in the request URL
+        //  to have a param matching what is in the annotation (ie: @RequestParam("startYear"))
+        return moviesRepository.findByYearRange(startYear, endYear);
+    }
+
+    @PostMapping // /api/movies POST
     public void create(@RequestBody Movie movie){
+        // add to our movies list (fake db)
         moviesRepository.save(movie);
     }
 
-
-    @PostMapping("all")  // /api/movies/many POST
-    public void createMany(@RequestBody List<Movie> movies){
-        System.out.println(movies);
+    @PostMapping ("many")// /api/movies/many POST
+    public void createMany(@RequestBody List<Movie> movies){ // @RequestBody is very important to knowing how the Request's body maps
+        // saveAll() lets you pass a collection as an argument and commit all the objects to the database
+        moviesRepository.saveAll(movies);
     }
 
+    @DeleteMapping("{id}") // api/movies/{id} -> api/movies/3 delete
+    public void deleteById(@PathVariable int id, HttpServletResponse response) throws IOException {
+        try{
+            moviesRepository.deleteById(id);
+        }catch (Exception ex){
+            response.sendError(200,"No movie matching ID: " + id);
+        }
 
-
-
-    private List<Movie> setMovies() {
-        List<Movie> movies = new ArrayList<>();
-
-        movies.add(new Movie(2, "Pulp Fiction", "1994", "Quentin Tarantino",
-                "Samuel L. Jackson, Uma Therman, Bruce Willis, John Travolta, Ving Rhames",
-                "10", "action, drama, suspense, cult classic, crime",
-                "Vincent Vega (John Travolta) and Jules Winnfield (Samuel L. Jackson) are hitmen with a penchant for philosophical discussions. " +
-                        "In this ultra-hip, multi-strand crime movie, their storyline is interwoven with those of their boss, " +
-                        "gangster Marsellus Wallace (Ving Rhames) ; his actress wife, Mia (Uma Thurman) ; " +
-                        "struggling boxer Butch Coolidge (Bruce Willis) ; master fixer Winston Wolfe (Harvey Keitel) and a nervous pair of armed robbers, " +
-                        "\"Pumpkin\" (Tim Roth) and \"Honey Bunny\" (Amanda Plummer)."));
-        movies.add(new Movie(1, "The Big Lebowski",
-                "1995", "The Cohen Bros",
-                "Jeff Bridges, John Goodman, Steve Buscemi",
-                "idk bro", "comedy, drama?",
-                "the dude just wanted to relax and go bowling"));
-
-        return movies;
     }
+
 }
